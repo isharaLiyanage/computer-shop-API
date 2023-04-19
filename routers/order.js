313d582401261f3cf3aa2b7bcc70b6d7167e13ae
ishara dulanjaya <107528780+isharaLiyanage@users.noreply.github.com>
2023-04-19 19:01:34 +0530

    s

--- routers/order.js
+++ routers/order.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Order = require("../models/Order");
+const Order = require("../Models/Order");
 const { json } = require("express");
 const {
   verifyToken,
